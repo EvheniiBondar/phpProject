c7d77ce078e5a688120630b29d35b26954d00e8a
EvheniiBondar <bondarzena75@gmil.com>
2021-09-24 23:47:52 +0300

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1>Heading</h1>
+<p>Hello</p>
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+echo 2+3 ;
+?>
+
+
